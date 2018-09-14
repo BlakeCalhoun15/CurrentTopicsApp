677d7f371f6ec20031c6bb409d7a2174c581ba6b
Lyra Lapuyade <llapuyad@samford.edu>
2018-09-14 12:37:23 -0500

    Transition from login to create user

--- UntitledGroceryApp/app/src/main/java/com/example/joseph/untitledgroceryapp/Login.java
+++ UntitledGroceryApp/app/src/main/java/com/example/joseph/untitledgroceryapp/Login.java
@@ -1,7 +1,12 @@
 package com.example.joseph.untitledgroceryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        final EditText editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        final Button buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        final TextView createAccountLink = (TextView) findViewById(R.id.textViewCreateAccount);
+
+        createAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent createAccountIntent = new Intent(Login.this, CreateAccount.class);
+                Login.this.startActivity(createAccountIntent);
+            }
+        });
+
     }
 }
