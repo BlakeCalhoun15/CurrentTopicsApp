7e447889b52baa42f254fd2d6053ed9e051641fc
Lyra Lapuyade <llapuyad@samford.edu>
2018-09-20 12:44:35 -0500

    Grocery App

---
+++ GroceryApp/app/src/main/java/com/example/joseph/untitledgroceryapp/Startup.java
@@ -0,0 +1,39 @@
+package com.example.joseph.untitledgroceryapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import services.AzureServiceAdapter;
+//import com.microsoft.windowsazure.mobileservices.*;
+
+public class Startup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+
+        /*try {
+            AzureServiceAdapter.Initialize(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),Login.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+            myThread.start();
+
+    }
+
+}
